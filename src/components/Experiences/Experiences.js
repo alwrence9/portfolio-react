f0e0e2411c478815d7f29bc080305c70a23f0bf1
Ashley Vu <ashleyvu2209@gmail.com>
2024-10-21 00:12:21 -0400

    Fix path of logos

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -9,11 +9,11 @@ function Experiences() {
                     <Experience company="LatinArte"
                                 description="Worked as a Software Developer for LatinArte. Updated their existing WordPress website and developed a mobile eshop."
                                 url="https://latinarte.ca/"
-                                logo="/images/logos/latinarte.png" />
+                                logo="images/logos/latinarte.png" />
                     <Experience company="Aplus"
                                 description="Developed a full-stack website using React and Node.js. Used AWS as hosting platform. Used Google Analytics to improve website click-through rate."
                                 url="https://aplustransition.com/en/"
-                                logo="/images/logos/aplus.svg" />
+                                logo="images/logos/aplus.svg" />
                 </div>
             </div>
         </section>
