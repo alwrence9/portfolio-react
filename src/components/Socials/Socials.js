e8268fdd9546ea0e6391749cfa89edf557aa757e
Ashley Vu <ashleyvu2209@gmail.com>
2024-10-20 23:37:34 -0400

    Add socials animated username

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,21 +1,29 @@
+import React from 'react';
+import './Socials.css';
+
 function Socials() {
     return (
         <div className="row social">
             <ul className="footer__social-links">
                 <li className="footer__social-link-item">
-                    <a href="https://github.com/alwrence9/" title="Link to Github Profile">
-                        <img src="./images/github.svg" className="footer__social-image" alt="Github"/>
-                    </a>
+                    <div className="hover-container">
+                        <a href="https://github.com/alwrence9/" title="Link to Github Profile">
+                            <img src="./images/github.svg" className="footer__social-image" alt="Github" />
+                            <span className="hover-text">alwrence9</span>
+                        </a>
+                    </div>
                 </li>
                 <li className="footer__social-link-item">
-                    <a href="https://www.linkedin.com/in/hong-minh-vu-77a072250/">
-                        <img src="./images/linkedin.svg" title="Link to Linkedin Profile"
-                             className="footer__social-image" alt="Linkedin"/>
-                    </a>
+                    <div className="hover-container">
+                        <a href="https://www.linkedin.com/in/hong-minh-vu-77a072250/" title="Link to Linkedin Profile">
+                            <img src="./images/linkedin.svg" className="footer__social-image" alt="Linkedin" />
+                            <span className="hover-text">hong-minh-vu</span>
+                        </a>
+                    </div>
                 </li>
             </ul>
         </div>
-    )
+    );
 }
 
 export default Socials;
